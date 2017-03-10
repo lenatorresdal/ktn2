6932bb468ba8b52407f11b6e07c56ed917c403dd
ingebofb <ingebofb@stud.ntnu.no>
2017-03-10 10:49:35 +0100

    Add files via upload

---
+++ MessageParser.py
@@ -0,0 +1,28 @@
+
+
+class MessageParser():
+    def __init__(self):
+
+        self.possible_responses = {
+            'error': self.parse_error,
+            'info': self.parse_info,
+            'message': self.parse_message,
+            'history': self.parse_history,
+        }
+
+    def parse(self, payload):
+        payload = # decode the JSON object
+
+        if payload['response'] in self.possible_responses:
+            return self.possible_responses[payload['response']](payload)
+        else:
+            # Response not valid
+
+    def parse_error(self, payload):
+    
+    def parse_info(self, payload):
+
+    def parse_message(self, payload):
+
+    def parse_history(self, payload):
+
